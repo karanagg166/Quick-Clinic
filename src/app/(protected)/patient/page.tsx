58610f4b888d42349e1fde8e33237410220ac5be
0harsh <hmishra1729@gmail.com>
2025-12-05 01:41:46 +0530

    added find doctor page

--- src/app/(protected)/patient/page.tsx
+++ src/app/(protected)/patient/page.tsx
@@ -1,5 +1,10 @@
 
 
 export default function PatientDashboard() {
-  return (<></>);
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold mb-4">Patient Dashboard</h2>
+      <p>Welcome to your patient dashboard! Here you can view your health summary, upcoming appointments, and recent activity.</p>
+    </div>
+  );
 };
